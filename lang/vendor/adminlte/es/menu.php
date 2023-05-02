4e6423e182f2668495596c4f2d206a89af739531
Natalia Romero <natalia.romero_g@mail.udp.cl>
2023-05-02 12:19:30 -0400

    first commit

---
+++ lang/vendor/adminlte/es/menu.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    'main_navigation'               => 'MENU PRINCIPAL',
+    'search'                        => 'Búsqueda',
+    'blog'                          => 'Blog',
+    'pages'                         => 'Páginas',
+    'account_settings'              => 'AJUSTES DE LA CUENTA',
+    'profile'                       => 'Perfil',
+    'change_password'               => 'Cambiar Contraseña',
+    'multilevel'                    => 'Multi Nivel',
+    'level_one'                     => 'Nivel 1',
+    'level_two'                     => 'Nivel 2',
+    'level_three'                   => 'Nivel 3',
+    'labels'                        => 'ETIQUETAS',
+    'important'                     => 'Importante',
+    'warning'                       => 'Advertencia',
+    'information'                   => 'Información',
+    'users'                         => 'USUARIOS',
+    'tickets'                       => 'TICKETS',
+    'statistics'                    => 'ESTADÍSTICAS',
+    'add_ticket'                    => 'Agregar Ticket',
+    'view_ticket'                   => 'Ver Tickets',
+    'view_user'                     => 'Ver Usuarios',
+    'add_user'                      => 'Agregar Usuarios',
+    'view_stats'                    => 'Ver Estadísticas',
+];
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,18 @@
+@extends('adminlte::page')
+@section('title', 'Dashboard')
+
+@section('css')
+
+@stop
+
+@section('content_header')
+    <h1>Página de inicio</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido a la gestión de tickets!</p>
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
