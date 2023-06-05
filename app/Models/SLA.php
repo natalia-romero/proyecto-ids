db5a1c9d344847ee2f27b00b341d2fda3d2147b7
Natalia Romero <natalia.romero_g@mail.udp.cl>
2023-06-05 16:42:18 -0400

    se crea cronjob para calcular sla

--- app/Models/SLA.php
+++ app/Models/SLA.php
@@ -8,10 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class SLA extends Model
 {
     use HasFactory;
-    public const LOW_ID = 1;
-    public const NORMAL_ID = 2;
+    public const NORMAL_ID = 1;
+    public const MEDIUM_ID = 2;
     public const HIGH_ID = 3;
-    public const URGENT_ID = 4;
     public function tickets()
     {
         return $this->hasMany(Ticket::class);
